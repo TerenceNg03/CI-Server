5e16c5dbb661eb7615aae3941048b5e5ebd44d0f
Terence Ng <terenceng03@outlook.com>
2024-02-03 11:12:41 +0100

    Add #1 base project set up (#4)
    * Set up project skeleton
    
    * Add style checker

---
+++ src/test/java/ci/AppTest.java
@@ -0,0 +1,15 @@
+package ci;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/** Unit test for simple App. */
+public class AppTest {
+    /** Rigorous Test :-) */
+    @Test
+    public void shouldAnswerWithTrue() {
+        App.main(null);
+        assertTrue(true);
+    }
+}
