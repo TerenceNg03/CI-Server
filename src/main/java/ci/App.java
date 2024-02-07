a25b4f306ceec0efaee30e76dfb25519c5b4051f
celin <celinat@kth.se>
2024-02-07 09:43:57 +0100

    add basic function to get json

--- src/main/java/ci/App.java
+++ src/main/java/ci/App.java
@@ -1,8 +1,42 @@
 package ci;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.json.simple.JSONObject;
+
 /** Hello world! */
 public class App {
+
+    /**
+     *
+     * @param file
+     */
+    public static void readInput(File file) {
+    }
+
+    /**
+     * Placeholder function for writing json to file
+     *
+     * @param jsonObj
+     */
+    public static void writeToFile(File file, JSONObject jsonObj) {
+        try {
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write(jsonObj.toJSONString());
+            fileWriter.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //String input = args[1];
+        //System.out.println(input);
+        File file = new File(".\\exampleOutput.json");
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("compile", "true");
+        writeToFile(file, jsonObject);
     }
 }
