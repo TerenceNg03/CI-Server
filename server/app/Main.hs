bb8c45f1cc0e007f7238c75be2bd8eb7dc78b516
TerenceNg03 <terenceng03@outlook.com>
2024-02-07 03:36:36 +0100

    Change file logger to handle logger

--- server/app/Main.hs
+++ server/app/Main.hs
@@ -2,10 +2,13 @@ module Main (main) where
 
 import Config (Config (logFile))
 import Data.Yaml (decodeFileThrow)
-import FileLogger (withFileLogger)
+import HandleLogger (withHandleLogger)
 import Server (runServer)
+import System.IO (IOMode (AppendMode), openFile, hClose)
 
 main :: IO ()
 main = do
     config <- decodeFileThrow "./config.yaml"
-    withFileLogger (logFile config) $ runServer config
+    handle <- openFile (logFile config) AppendMode
+    withHandleLogger handle $ runServer config
+    hClose handle
---
+++ server/src/HandleLogger.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module HandleLogger (withHandleLogger) where
+
+import Control.Monad.IO.Unlift (MonadUnliftIO (withRunInIO))
+import qualified Data.Text.IO as T
+import System.IO (Handle)
+
+import Log (Logger, mkBulkLogger, showLogMessage)
+import Log.Internal.Logger (withLogger)
+import qualified GHC.IO.Handle as T
+
+-- | Print log to a handle with flushing on each write
+withHandleLogger :: (MonadUnliftIO m) => Handle -> (Logger -> m r) -> m r
+withHandleLogger handle act = withRunInIO $ \unlift -> do
+    logger <-
+        mkBulkLogger
+            "file-bulk"
+            ( \msgs -> do
+                mapM_ (T.hPutStrLn handle . showLogMessage Nothing) msgs
+                T.hFlush handle
+            )
+            (return ())
+    withLogger logger (unlift . act)
