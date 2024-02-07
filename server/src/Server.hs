0be6fb3c0516eb9d225339f6bfef3dbb0efa31d5
TerenceNg03 <terenceng03@outlook.com>
2024-02-07 09:55:34 +0100

    Improve request logging

--- server/src/Server.hs
+++ server/src/Server.hs
@@ -5,33 +5,37 @@
 module Server (runServer) where
 
 import Config (Config (..))
-import Control.Exception (bracketOnError)
+import Data.Maybe (fromMaybe)
 import Fmt (format)
 import Log (LogT, defaultLogLevel, logInfo_, runLogT)
 import Log.Logger (Logger)
-import Network.Socket (Family (AF_INET), SockAddr (SockAddrInet), SocketType (Stream), bind, close, listen, socket, socketPort, tupleToHostAddress, withSocketsDo)
-import Network.Wai (Request (rawPathInfo, remoteHost), requestMethod)
-import Web.Scotty.Trans (ScottyT, defaultOptions, function, matchAny, next, request, scottySocketT)
+import Network.Wai (Request (rawPathInfo, remoteHost, requestHeaderHost), requestMethod, strictRequestBody)
+import Web.Scotty.Trans (ScottyT, function, liftIO, matchAny, next, request, scottyT)
 
 -- | Dispatch requests based on patterns
 dispatch :: Config -> ScottyT (LogT IO) ()
 dispatch Config{} = do
     matchAny (function $ const $ Just []) $ do
         r <- request
-        logInfo_ $ format "{} {} {}" (show $ remoteHost r) (show $ requestMethod r) (show $ rawPathInfo r)
+        body <- liftIO $ strictRequestBody r
+        logInfo_ $
+            format
+                "{} {} {}\n    Header: {}\n    Body: {}"
+                (show $ remoteHost r)
+                (show $ requestMethod r)
+                (show $ rawPathInfo r)
+                (show $ fromMaybe "" $ requestHeaderHost r)
+                (show body)
         next
 
 -- | Run web server with given config and logger
 runServer :: Config -> Logger -> IO ()
-runServer config@Config{..} logger = withSocketsDo $ bracketOnError newSocket close $ \sock -> do
-    bind sock $ SockAddrInet (fromIntegral portNumber) $ tupleToHostAddress (0, 0, 0, 0)
-    listen sock 1024
-    port <- socketPort sock
+runServer config@Config{..} logger = do
     runLog $
         do
-            logInfo_ $ format "Running server on 0.0.0.0:{}" (show port)
+            logInfo_ $ format "Running server on 0.0.0.0:{}" (show portNumber)
             logInfo_ $ format "Database file path: {}" dbFile
-            scottySocketT defaultOptions sock runLog (dispatch config)
+            logInfo_ $ format "Log file path: {}" logFile
+            scottyT portNumber runLog (dispatch config)
   where
-    newSocket = socket AF_INET Stream 0
     runLog = runLogT "main" logger defaultLogLevel
