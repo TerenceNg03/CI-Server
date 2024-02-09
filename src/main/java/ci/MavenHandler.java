4135c90f36a45ada7f0db9c599de33630392eb0d
angen <angelica.princess@hotmail.com>
2024-02-09 17:00:52 +0100

    [feat] add MavenHandler class for mvn handling #9

---
+++ src/main/java/ci/MavenHandler.java
@@ -0,0 +1,9 @@
+package ci;
+
+/** MavenHandler class responsible for build and test of repository */
+public class MavenHandler {
+  private String dir;
+  public MavenHandler(final String dir) {
+    this.dir = dir;
+  }
+}
