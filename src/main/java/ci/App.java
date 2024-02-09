2b80de330b99f1a9846e2c08c8f8ac51ddf31324
celin <celinat@kth.se>
2024-02-09 08:41:58 +0100

    add simple test and clean up code

--- src/main/java/ci/App.java
+++ src/main/java/ci/App.java
@@ -10,47 +10,48 @@ public class App {
      *
      * @param dir the directory in which mvn package will execute
      * @return String[], storing the build status at index 0 and build log at index 1
-     * @throws Exception
+     * @throws IOException
      */
-    public String[] runBuild(String dir) throws Exception {
+    public String[] runBuild(String dir) throws IOException {
         String[] result = new String[2];
-        final String mavenCmd = System.getProperty("os.name").toLowerCase().contains("windows") ? "mvn.cmd" : "mvn";
+        final String mavenCmd =
+                System.getProperty("os.name").toLowerCase().contains("windows") ? "mvn.cmd" : "mvn";
         String[] command = {mavenCmd, "-f", dir, "package"};
         Process process = Runtime.getRuntime().exec(command);
         StringBuilder sb = new StringBuilder();
-        new Thread(() -> {
-            BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line = null;
-            try {
-                while ((line = input.readLine()) != null) {
-                    sb.append(line).append("\n");
-                }
-            }catch (IOException e ) {
-                e.printStackTrace();
-            }
-        }).start();
+        new Thread(
+                        () -> {
+                            BufferedReader input =
+                                    new BufferedReader(
+                                            new InputStreamReader(process.getInputStream()));
+                            String line;
+                            try {
+                                while ((line = input.readLine()) != null) {
+                                    sb.append(line).append("\n");
+                                }
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        })
+                .start();
         try {
             process.waitFor();
+            String message = sb.toString();
             if (process.exitValue() != 0) {
-                String message = sb.toString();
-                //System.out.println("Build failed \n" + process.exitValue()+ " " + message);
                 result[0] = "Fail";
                 result[1] = message;
-            }
-            else {
-                String message = sb.toString();
-                //System.out.println("Build was successful \n" + message);
+            } else {
                 result[0] = "Success";
                 result[1] = message;
             }
-        } catch (InterruptedException e ) {
+        } catch (InterruptedException e) {
 
             e.printStackTrace();
         }
         return result;
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World!");
         App app = new App();
         String dirWithBuild = "./";
--- src/test/java/ci/AppTest.java
+++ src/test/java/ci/AppTest.java
@@ -1,15 +1,27 @@
 package ci;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
+import java.io.IOException;
 import org.junit.Test;
 
 /** Unit test for simple App. */
-public class AppTest {
+public class AppTest extends App {
     /** Rigorous Test :-) */
     @Test
     public void shouldAnswerWithTrue() {
-        //App.main(null);
+        // App.main(null);
         assertTrue(true);
+    }
+
+    @Test
+    public void runBuildShouldFailIfFaultyDirectory() throws IOException {
+        String[] result = runBuild("...");
+        assertTrue(result[0].equals("Fail"));
+    }
+
+    // @Test
+    public void runBuildAlwaysFailTest() {
+        assertTrue(false);
     }
 }
