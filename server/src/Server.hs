9c43cbabed7769d9fa28aebca99db9457b9f7c7e
TerenceNg03 <terenceng03@outlook.com>
2024-02-09 02:56:35 +0100

    Add signature verfication

--- server/src/Server.hs
+++ server/src/Server.hs
@@ -5,28 +5,60 @@
 module Server (runServer) where
 
 import Config (Config (..))
-import Data.Maybe (fromMaybe)
+import Crypto.Hash.SHA256 (hmac)
+import Data.ByteString (toStrict)
+import Data.ByteString.Builder (byteStringHex, toLazyByteString)
+import Data.Foldable (find)
+import Data.Text.Encoding (decodeUtf8, encodeUtf8)
+import Data.Text.Lazy (fromStrict)
 import Fmt (format)
 import Log (LogT, defaultLogLevel, logInfo_, runLogT)
 import Log.Logger (Logger)
-import Network.Wai (Request (rawPathInfo, remoteHost, requestHeaderHost), requestMethod, strictRequestBody)
-import Web.Scotty.Trans (ScottyT, function, liftIO, matchAny, next, request, scottyT)
+import Network.HTTP.Types (status403)
+import Network.Wai (Request (rawPathInfo, remoteHost), requestMethod)
+import Web.Scotty.Trans (
+    ScottyT,
+    body,
+    function,
+    headers,
+    html,
+    matchAny,
+    next,
+    post,
+    request,
+    scottyT,
+    status,
+    text,
+ )
 
 -- | Dispatch requests based on patterns
 dispatch :: Config -> ScottyT (LogT IO) ()
-dispatch Config{} = do
+dispatch Config{..} = do
     matchAny (function $ const $ Just []) $ do
         r <- request
-        body <- liftIO $ strictRequestBody r
+        h <- headers
+        b <- body
         logInfo_ $
             format
                 "{} {} {}\n    Header: {}\n    Body: {}"
                 (show $ remoteHost r)
                 (show $ requestMethod r)
                 (show $ rawPathInfo r)
-                (show $ fromMaybe "" $ requestHeaderHost r)
-                (show body)
+                (show h)
+                (show b)
         next
+    post "/" $ do
+        h <- headers
+        payload <- body
+        let sig = hmac (encodeUtf8 webSecret) $ toStrict payload
+            decodeHex = decodeUtf8 . toStrict . toLazyByteString . byteStringHex
+            verify = find (== ("X-Hub-Signature-256", fromStrict $ "sha256=" <> decodeHex sig)) h
+        case verify of
+            Nothing -> do
+                logInfo_ "Signature verification failed"
+                status status403
+                html "<h1>Invalid Signature</h1>"
+            Just _ -> text ""
 
 -- | Run web server with given config and logger
 runServer :: Config -> Logger -> IO ()
