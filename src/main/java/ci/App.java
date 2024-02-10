ef7b2d29bc2d8f33ff59fafff156479e702f3700
angen <angelica.princess@hotmail.com>
2024-02-10 14:38:35 +0100

    fix spotless style #9

--- src/main/java/ci/App.java
+++ src/main/java/ci/App.java
@@ -5,46 +5,43 @@ import java.io.FileWriter;
 import java.io.IOException;
 import org.json.simple.JSONObject;
 
-/**
- * A class for the App.
- */
+/** A class for the App. */
 public class App {
 
-  /**
-   * Placeholder function for writing json to file.
-   *
-   * @param jsonObj the json object
-   */
-  public static void writeToFile(File file, JSONObject jsonObj) {
-    try {
-      FileWriter fileWriter = new FileWriter(file);
-      fileWriter.write(jsonObj.toJSONString());
-      fileWriter.close();
+    /**
+     * Placeholder function for writing json to file.
+     *
+     * @param jsonObj the json object
+     */
+    public static void writeToFile(File file, JSONObject jsonObj) {
+        try {
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write(jsonObj.toJSONString());
+            fileWriter.close();
 
-    } catch (IOException e) {
-      e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-  }
 
-  /**
-   * The main class of the app.
-   *
-   * @param args arguments for the program
-   * @throws IOException if the input fails
-   */
-  @SuppressWarnings("checkstyle:MagicNumber")
-  public static void main(String[] args) throws IOException {
-    String dirWithBuild = args[0];
+    /**
+     * The main class of the app.
+     *
+     * @param args arguments for the program
+     * @throws IOException if the input fails
+     */
+    public static void main(String[] args) throws IOException {
+        String dirWithBuild = args[0];
 
-    MavenHandler mavenHandler = MavenHandler.getInstance(dirWithBuild);
-    System.out.println("Result of compilation: " + mavenHandler.compileProgram());
-    System.out.println(" " + mavenHandler.getCompileLog());
-    System.out.println("Result of tests: " + mavenHandler.runTests());
-    System.out.println(" " + mavenHandler.getTestLog());
+        MavenHandler mavenHandler = MavenHandler.getInstance(dirWithBuild);
+        System.out.println("Result of compilation: " + mavenHandler.compileProgram());
+        System.out.println(" " + mavenHandler.getCompileLog());
+        System.out.println("Result of tests: " + mavenHandler.runTests());
+        System.out.println(" " + mavenHandler.getTestLog());
 
-    //        File file = new File(".\\exampleOutput.json");
-    //        JSONObject jsonObject = new JSONObject();
-    //        jsonObject.put("compile", "true");
-    //        writeToFile(file, jsonObject);
-  }
+        //        File file = new File(".\\exampleOutput.json");
+        //        JSONObject jsonObject = new JSONObject();
+        //        jsonObject.put("compile", "true");
+        //        writeToFile(file, jsonObject);
+    }
 }
