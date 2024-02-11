c1a2b01aa4584f27b8715bb3d6f62afaff9363f4
jsoderholm <soederholm.john@gmail.com>
2024-02-11 13:35:45 +0100

    Post Testing & Compilation Status

--- server/src/WebHook.hs
+++ server/src/WebHook.hs
@@ -10,6 +10,7 @@ import Control.Concurrent (forkIO)
 import Control.Exception
 import Control.Monad (void)
 import Control.Monad.Error.Class (MonadError (catchError))
+import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans (lift)
 import Data.Aeson (
     FromJSON,
@@ -33,9 +34,8 @@ import Fmt (format)
 import GHC.Generics (Generic)
 import Log (LogT, Logger, defaultLogLevel, logAttention_, logInfo_, runLogT)
 import Network.HTTP.Req (POST (POST), ReqBodyJson (ReqBodyJson), bsResponse, defaultHttpConfig, header, https, req, responseBody, runReq, (/:))
-import System.Directory (setCurrentDirectory)
-import System.IO
-import System.Process
+import System.Exit (ExitCode (..))
+import System.Process (cwd, readCreateProcessWithExitCode, shell)
 
 -- | Commit Info
 data Commit = Commit
@@ -98,14 +98,13 @@ instance Show State where
         Pending -> "pending"
         Success -> "success"
 
-invokeMavenCommand :: String -> IO String
+invokeMavenCommand :: String -> IO (Either String (Int, String))
 invokeMavenCommand command = do
-    setCurrentDirectory ".."
-    (_, Just hout, _, ph) <- createProcess (proc "mvn -q" [command]){std_out = CreatePipe, std_err = NoStream}
-    output <- hGetContents hout
-    _ <- waitForProcess ph
-    setCurrentDirectory "server"
-    return output
+    let mvnCommand = shell $ unwords ["mvn", "-q", command]
+    result <- readCreateProcessWithExitCode mvnCommand{cwd = Just "../"} ""
+    return $ case result of
+        (ExitSuccess, stdout, _) -> Right (0, stdout)
+        (ExitFailure code, _, stderr) -> Left (stderr ++ "\nExit code: " ++ show code)
 
 postStatus :: UUID -> Text -> Text -> Commit -> State -> Text -> LogT IO ()
 postStatus uuid token domain commit s desc = flip catchError (logInfo_ . pack . show) $ do
@@ -141,16 +140,7 @@ postStatus uuid token domain commit s desc = flip catchError (logInfo_ . pack . 
 
 -- | Run webhook jobs and post status to github
 runWebHook :: UUID -> Logger -> Config -> Commit -> IO ()
-runWebHook uuid logger Config{..} commit = do
-    compOutput <- invokeMavenCommand "compile"
-    currentTime <- getCurrentTime
-    runSql $
-        insertBuild $
-            Build
-                (toText uuid)
-                (after commit)
-                (pack $ formatTime defaultTimeLocale "%F %T %z" currentTime)
-                (pack compOutput)
+runWebHook uuid logger config@Config{..} commit = do
     void
         $ forkIO
         $ runLogT
@@ -159,7 +149,45 @@ runWebHook uuid logger Config{..} commit = do
             defaultLogLevel
         $ do
             postStatus' Pending "Working on checks..."
-            postStatus' Success $ pack compOutput
+            handleCompileResult uuid commit config
+            handleTestingResult uuid commit config
+  where
+    postStatus' = postStatus uuid githubToken domain commit
+
+handleCompileResult :: UUID -> Commit -> Config -> LogT IO ()
+handleCompileResult uuid commit Config{..} = do
+    postStatus' Pending "Compilation in progress..."
+    compileResult <- liftIO $ invokeMavenCommand "compile"
+    case compileResult of
+        Left errMsg -> do
+            logAttention_ $ format "Failed to compile: {}" errMsg
+            postStatus' Error "Compilation failed!"
+        Right (exitCode, compileOutput) -> do
+            currentTime <- liftIO getCurrentTime
+            liftIO $
+                runSql $
+                    insertBuild $
+                        Build
+                            (toText uuid)
+                            (after commit)
+                            (pack $ formatTime defaultTimeLocale "%F %T %z" currentTime)
+                            (pack compileOutput)
+            logInfo_ $ format "Compilation succeeded with exit code: {}" exitCode
+            postStatus' Success "Compilation succeeded!"
   where
     postStatus' = postStatus uuid githubToken domain commit
     runSql = runSqlite $ pack dbFile
+
+handleTestingResult :: UUID -> Commit -> Config -> LogT IO ()
+handleTestingResult uuid commit Config{..} = do
+    postStatus' Pending "Testing in progress..."
+    testResult <- liftIO $ invokeMavenCommand "test"
+    case testResult of
+        Left errMsg -> do
+            logAttention_ $ format "Failed to test: {}" errMsg
+            postStatus' Error "Testing failed!"
+        Right (exitCode, _) -> do
+            logInfo_ $ format "Testing succeeded with exit code: {}" exitCode
+            postStatus' Success "Testing succeeded!"
+  where
+    postStatus' = postStatus uuid githubToken domain commit
