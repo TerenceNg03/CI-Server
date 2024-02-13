cfbc1036d881297339b6a1f0229050e8eebb2b97
Angelica Engström <78786414+angelicaengstrom@users.noreply.github.com>
2024-02-13 10:51:57 +0100

    Add #29 output of result to console (#34)

--- src/main/java/ci/App.java
+++ src/main/java/ci/App.java
@@ -1,26 +1,25 @@
 package ci;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import org.json.simple.JSONObject;
 
 /** A class for the App. */
 public class App {
 
     /**
-     * Placeholder function for writing json to file.
+     * A function to stop the execution with a certain code. If the exit status is 1 the compilation
+     * failed and the tests couldn't build. If the exit status is 2 the tests failed. If the exit
+     * status is 0 everything is OK.
      *
-     * @param jsonObj the json object
+     * @param compileStatus the status of the compilation
+     * @param testStatus the status of the test
      */
-    public static void writeToFile(File file, JSONObject jsonObj) {
-        try {
-            FileWriter fileWriter = new FileWriter(file);
-            fileWriter.write(jsonObj.toJSONString());
-            fileWriter.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static void exitSystem(final boolean compileStatus, final boolean testStatus) {
+        if (!compileStatus) {
+            System.exit(1);
+        } else if (!testStatus) {
+            System.exit(2);
+        } else {
+            System.exit(0);
         }
     }
 
@@ -34,14 +33,11 @@ public class App {
         String dirWithBuild = args[0];
 
         MavenHandler mavenHandler = MavenHandler.getInstance(dirWithBuild);
-        System.out.println("Result of compilation: " + mavenHandler.compileProgram());
-        System.out.println(" " + mavenHandler.getCompileLog());
-        System.out.println("Result of tests: " + mavenHandler.runTests());
-        System.out.println(" " + mavenHandler.getTestLog());
+        boolean compileStatus = mavenHandler.compileProgram();
+        boolean testStatus = mavenHandler.runTests();
 
-        //        File file = new File(".\\exampleOutput.json");
-        //        JSONObject jsonObject = new JSONObject();
-        //        jsonObject.put("compile", "true");
-        //        writeToFile(file, jsonObject);
+        System.out.println(mavenHandler.getTestLog());
+
+        exitSystem(compileStatus, testStatus);
     }
 }
